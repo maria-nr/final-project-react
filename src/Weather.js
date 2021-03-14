9479d6e0a56eb732cc7be2b628765d0c1f72753d
maria-nr <66881646+maria-nr@users.noreply.github.com>
2021-03-14 20:02:34 +0100

    Search engine
    Working with all the city, also fix some names

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import "./Weather.css";
 import axios from "axios";
-import ReactLoading from 'react-loading';
 import ActualDate from "./ActualDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
 const [data, setData]= useState({ready:false});
+const [city, setCity]= useState(props.defaultCity);
     function handleResponse (response) {
         setData({
             ready: true,
@@ -17,13 +18,30 @@ const [data, setData]= useState({ready:false});
            date: new Date(response.data.dt * 1000),
         })
     }
+
+    function search() {
+        const apiKey ="c522524629fe11fe83aebe5bb3814efa";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+
+    }
+
+    function handleCitychange(event){
+        setCity(event.target.value);
+
+    }
     if (data.ready) {
         return (
             <div className="weather">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-11">
-                    <input type="search" placeholder="Find your city" className="form-control" />
+                    <input type="search" placeholder="Find your city" className="form-control"  onChange={handleCitychange}/>
                         </div>
                         <div className ="col-1">
                     <input type="submit" value= "🔎" className="btn btn-info " />
@@ -33,47 +51,15 @@ const [data, setData]= useState({ready:false});
                     </p>
                     </div>
                 </form>
-                <div className="background">
-                        
-                <div className="top-tittle">
-                <h4> {data.city} </h4>
-                <div><h1> <span className="temperature">{Math.round(data.temperature)}</span><span className="unit">ºC</span><span className="unit">ºF</span></h1> </div>
-                <p className="text-capitalize"> {data.description} </p> 
-                </div>
-                <div className="icon">
-                    <img src={data.icon} alt={data.description} className="float right"  />
-
-                </div>
-    
-                <div className="row">
-                    <div className="details">
-                    <div className="col-4">
-                    <ul>
-                        <li className="title-details"> Humidity</li>
-                        <li className="detail">{data.humidity}%</li>
-                        <li className="title-details"> Wind</li>
-                        <li className="detail">{data.wind}km/h</li>
-                    </ul> 
-                    </div>
-                    </div>
-                    <div className="col-8">
-                    <div className="clearfix">
-                    
-                    </div>
-                    </div>
-                </div>    
-                </div>
+                <WeatherInfo data={data}/>
+              
             </div>
             
         );
 
     } else {
 
-    
-    const apiKey ="c522524629fe11fe83aebe5bb3814efa";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+        search();
     return "loading...";
 }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+ export default function WeatherInfo (props) {
+     return (
+         <div className="WeatherInfo">
+               <div className="background">
+                        
+                        <div className="top-tittle">
+                        <h4> {props.data.city} </h4>
+                        <div><h1> <span className="temperature">{Math.round(props.data.temperature)}</span><span className="unit">ºC</span><span className="unit">ºF</span></h1> </div>
+                        <p className="text-capitalize"> {props.data.description} </p> 
+                        </div>
+                        <div className="icon">
+                            <img src={props.data.icon} alt={props.data.description} className="float right"  />
+        
+                        </div>
+            
+                        <div className="row">
+                            <div className="details">
+                            <div className="col-4">
+                            <ul>
+                                <li className="title-details"> Humidity</li>
+                                <li className="detail">{props.data.humidity}%</li>
+                                <li className="title-details"> Wind</li>
+                                <li className="detail">{props.data.wind}km/h</li>
+                            </ul> 
+                            </div>
+                            </div>
+                            <div className="col-8">
+                            <div className="clearfix">
+                            
+                            </div>
+                            </div>
+                        </div>    
+                        </div>
+         </div>
+     );
+ }
